b44498be297db81415398bc51ac7dc97866a5376
mimi-uxui-dev <uxui.mimi@gmail.com>
2023-07-14 11:27:23 +0100

    footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,13 +3,31 @@ import React from "react";
 function Footer() {
   return (
     <footer className="container mb200">
-      <div>
-        <h6>Contact</h6>
-        <a href="mailto:team@founderdesign.io" target="_blank" rel="noreferrer">
-          team@founderdesign.io
-        </a>
+      <div className="footer-top">
+        <div>
+          <h6>Contact</h6>
+          <a
+            href="mailto:team@founderdesign.io"
+            target="_blank"
+            rel="noreferrer"
+          >
+            team@founderdesign.io
+          </a>
+        </div>
+        <p>
+          FounderDesign.io, Gubelstrasse 15, <br />
+          6300 Zug, Switzerland
+        </p>
       </div>
-      <p>©FounderDesign</p>
+
+      <div className="footer-bottom">
+        <div>
+          <a href="/policies/privacy">Privacy Policy</a>{" "}
+          <a href="/policies/terms">Terms of Service</a>
+          <a href="/policies/imprint">Impressum</a>
+        </div>
+        <p>©FounderDesign</p>
+      </div>
     </footer>
   );
 }
